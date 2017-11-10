87080c096c4f0b12a42fd7570a64c8a96b7d7a18
Juan Tito <tito.lopez.juan@gmail.com>
2017-11-10 17:59:08 -0500

    Basic example

---
+++ README.txt
@@ -0,0 +1,13 @@
+Pre-requisitos: NodeJS
+
+Steps:
+1. Create folder "test"
+2. Run in cmd "npm init -y" and then "npm i -D webpack"
+3. Add key and valu in scripts {} "/package.json"
+	"dev": "node_modules/.bin/webpack index.js bundle.js"
+4. Generate configuration file "webpack.config.js"
+5. Create constant "const path = require('path');"
+	Path > It serves for the routing of files in different operating systems
+
+CMD: In your folder proyect run "npm run dev"
+	dev > Step 3
---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
+  template: './index.html',
+  filename: 'index.html',
+  inject: 'body'
+})
+
+
+const CONFIG = {
+  entry: './index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [
+      { test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ },
+      { test: /\.jsx$/, loader: 'babel-loader', exclude: /node_modules/ }
+    ]
+  },
+  plugins: [HtmlWebpackPluginConfig]
+}
+
+module.exports = CONFIG;
